237378b9b98b61302c6e432c32b85046b0b7ecd5
Adnan Radwan <adnanradwan_8@hotmail.com>
2024-04-21 15:15:55 -0400

    gotta do run map

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,66 +1,76 @@
 <script lang="ts">
-    import "../app.pcss";
-    import { toast, Toaster } from 'svelte-french-toast';
-    import Sidebar from './(components)/Sidebar.svelte'
-    import { cn } from "$lib/utils"
-	import Button from "$lib/components/ui/button/button.svelte";
-    import Icon from "$lib/components/ui/icon/Icon.svelte";
-    import { page } from "$app/stores";
-    import { fade } from 'svelte/transition';
-    import { afterNavigate } from '$app/navigation';
+	import '../app.pcss';
+	import { toast, Toaster } from 'svelte-french-toast';
+	import Sidebar from './(components)/Sidebar.svelte';
+	import { cn } from '$lib/utils';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Icon from '$lib/components/ui/icon/Icon.svelte';
+	import { page } from '$app/stores';
+	import { fade } from 'svelte/transition';
+	import { afterNavigate } from '$app/navigation';
 
-    afterNavigate(() => {
-        document.getElementById('main')?.scrollTo(0, 0);
-    });
+	afterNavigate(() => {
+		document.getElementById('main')?.scrollTo(0, 0);
+	});
 
-    export let data
-    let { supabase } = data
-    $: ({ supabase } = data)
+	export let data;
+	let { supabase } = data;
+	$: ({ supabase } = data);
 
-    $: if ($page.url.searchParams.has('error_description')) {
-    const errorDescription = $page.url.searchParams.get('error_description');
+	$: if ($page.url.searchParams.has('error_description')) {
+		const errorDescription = $page.url.searchParams.get('error_description');
 
-    if (errorDescription) {
-       toast.error(decodeURIComponent(errorDescription));
-    }
-}
+		if (errorDescription) {
+			toast.error(decodeURIComponent(errorDescription));
+		}
+	}
 
-   let openNav = false
+	let openNav = false;
 
-   const toggleOpenNav = () => {
-        openNav = !openNav
-    }
+	const toggleOpenNav = () => {
+		openNav = !openNav;
+	};
 
-    $: $page.url.pathname, openNav = false;
-
+	$: $page.url.pathname, (openNav = false);
 </script>
 
-<Toaster/>
-<div class="md:hidden flex items-center w-full h-16 p-2 justify-between border fixed top-0 shadow-sm z-10 bg-white">
-    <Button on:click={toggleOpenNav}><Icon icon="material-symbols:menu"/></Button>
-    <Icon icon="noto:running-shoe"/>
+<Toaster />
+<div
+	class="fixed top-0 z-10 flex h-16 w-full items-center justify-between border bg-white p-2 shadow-sm md:hidden"
+>
+	<Button on:click={toggleOpenNav}><Icon icon="material-symbols:menu" /></Button>
+	<Icon icon="noto:running-shoe" />
 </div>
 
-<div class="flex md:flex-row bg-slate-100 w-full h-full p-2 gap-2 mt-16 md:mt-0">
-    <aside class={cn(
-        openNav ? "left-0" : "left-[-300%]", 
-        "h-full fixed top-0 z-10 w-[300px]",
-        "md:w-[400px] md:left-0 md:relative md:h-[calc(100vh-1rem)]"
-    )}>
-        <Sidebar/>
-    </aside>
+<div class="mt-16 flex h-full w-full gap-2 bg-slate-100 p-2 md:mt-0 md:flex-row">
+	<aside
+		class={cn(
+			openNav ? 'left-0' : 'left-[-300%]',
+			'fixed top-0 z-10 h-full w-[300px]',
+			'md:relative md:left-0 md:h-[calc(100vh-1rem)] md:w-[400px]'
+		)}
+	>
+		<Sidebar />
+	</aside>
 
-    {#if openNav}
-        <button type="button" aria-label="close sidebar" in:fade={{ duration: 300 }} class="fixed top-0 left-[300px] md:hidden w-full h-screen bg-black/50 z-20" on:click={toggleOpenNav} on:keydown={toggleOpenNav}></button>
-    {/if}
+	{#if openNav}
+		<button
+			type="button"
+			aria-label="close sidebar"
+			in:fade={{ duration: 300 }}
+			class="fixed inset-0 left-[300px] top-0 z-50 h-screen w-full bg-background/80 backdrop-blur-sm md:hidden"
+			on:click={toggleOpenNav}
+			on:keydown={toggleOpenNav}
+		></button>
+	{/if}
 
-    <main id="main"class="overflow-x-hidden w-full h-[calc(100vh-1rem)] overflow-y-auto">
-        <slot/>
-    </main>
+	<main id="main" class="h-[calc(100vh-1rem)] w-full overflow-y-auto overflow-x-hidden">
+		<slot />
+	</main>
 </div>
 
 <style>
-    aside {
-        transition: left 0.3s ease-in-out; /* Adjust duration and easing as desired */
-    }
+	aside {
+		transition: left 0.3s ease-in-out; /* Adjust duration and easing as desired */
+	}
 </style>
--- src/routes/shoes/(components)/Filter.svelte
+++ src/routes/shoes/(components)/Filter.svelte
@@ -1,45 +1,65 @@
 <script lang="ts">
-    import * as Dialog from '$lib/components/ui/dialog';
-	import * as Tabs from "$lib/components/ui/tabs";
-    import Button from '$lib/components/ui/button/button.svelte';
-    import { page } from '$app/stores'
+	import * as Dialog from '$lib/components/ui/dialog';
+	import * as Tabs from '$lib/components/ui/tabs';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { page } from '$app/stores';
 
 	import SpecsForm from './SpecsForm.svelte';
 	import RunsForm from './RunsForm.svelte';
 
-    let selectedTab = 'shoe details';
-    let dialogOpen = false
+	let selectedTab = 'shoe details';
+	let dialogOpen = false;
 
-    const tabs = ['shoe details', 'run type']
+	const tabs = ['shoe details', 'run type'];
+
+	const handleClearFilter = () => {};
 </script>
 
-<Dialog.Root bind:open={dialogOpen} >
-    <Dialog.Trigger class="font-sm rounded-lg border bg-primary px-4 py-2 font-semibold text-white" on:click={() => (dialogOpen = true)}
-        >Filter
-    </Dialog.Trigger>
-    <Dialog.Content class="overflow-y-auto max-h-[calc(100vh-2rem)] max-w-[calc(100vw-2rem)] lg:max-w-[500px]">
-        <Dialog.Header>
-            <Dialog.Title>Filter By:</Dialog.Title>
-        </Dialog.Header>
+<Dialog.Root bind:open={dialogOpen}>
+	<Dialog.Trigger
+		class="font-sm rounded-lg border bg-primary px-4 py-2 font-semibold text-white"
+		on:click={() => (dialogOpen = true)}
+		>Filter
+	</Dialog.Trigger>
+	<Dialog.Content
+		class="max-h-[calc(100vh-2rem)] max-w-[calc(100vw-2rem)] overflow-y-auto lg:max-w-[500px]"
+	>
+		<Dialog.Header>
+			<Dialog.Title>Filter By:</Dialog.Title>
+		</Dialog.Header>
 
-        <Tabs.Root value={selectedTab} onValueChange={(v) => {if (v) selectedTab = v}} class="w-full">
-            <Tabs.List class="w-full justify-between">
-                {#each tabs as tab}
-                    <Tabs.Trigger value={tab} class="capitalize flex-grow">{tab}</Tabs.Trigger>
-                {/each}
-            </Tabs.List>
-            <Tabs.Content value="shoe details" class="text-muted-foreground">Know what type of shoe you want or just want to compare your options? Filter shoes by their specifications.</Tabs.Content>
-            <Tabs.Content value="run type" class="text-muted-foreground">Need a shoe to get into running or complete your collection? Filter shoes by your running needs.</Tabs.Content>
-        </Tabs.Root>
+		<Tabs.Root
+			value={selectedTab}
+			onValueChange={(v) => {
+				if (v) selectedTab = v;
+			}}
+			class="w-full"
+		>
+			<Tabs.List class="w-full justify-between">
+				{#each tabs as tab}
+					<Tabs.Trigger value={tab} class="flex-grow capitalize">{tab}</Tabs.Trigger>
+				{/each}
+			</Tabs.List>
+			<Tabs.Content value="shoe details" class="text-muted-foreground"
+				>Know what type of shoe you want or just want to compare your options? Filter shoes by their
+				specifications.</Tabs.Content
+			>
+			<Tabs.Content value="run type" class="text-muted-foreground"
+				>Need a shoe to get into running or complete your collection? Filter shoes by your running
+				needs.</Tabs.Content
+			>
+		</Tabs.Root>
 
-        <form method="GET" class="flex flex-col gap-6">
-            {#if selectedTab === 'shoe details'}
-                <SpecsForm />
+		<form method="GET" class="flex flex-col gap-6">
+			{#if selectedTab === 'shoe details'}
+				<SpecsForm />
 			{:else}
-				<RunsForm/>
+				<RunsForm />
 			{/if}
-
-            <Button type="submit" on:click={() => (dialogOpen = false)}>Search</Button>
-        </form>
-    </Dialog.Content>
+			<div class="flex flex-col gap-2">
+				<Button type="submit" on:click={() => (dialogOpen = false)}>Search</Button>
+				<Button href="/shoes" variant="ghost" on:click={() => (dialogOpen = false)}>Clear</Button>
+			</div>
+		</form>
+	</Dialog.Content>
 </Dialog.Root>
